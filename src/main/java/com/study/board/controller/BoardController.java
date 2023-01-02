5e1535ea729123d0b3f61c80a57543e9e35c76a5
khoney0929 <khoney0929@gmail.com>
2023-01-02 23:44:36 +0900

    make a board by read

---
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,50 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+
+
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write") //localhost:8080/boardWrite
+    public String boardWriteForm() {
+
+        return "boardWrite";
+    }
+    @PostMapping("/board/writePro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+
+        model.addAttribute("list",boardService.boardList());
+
+        return "boardList";
+    }
+    @GetMapping("/board/view") // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board",boardService.boardView(id));
+
+
+        return "boardView";
+    }
+
+}
